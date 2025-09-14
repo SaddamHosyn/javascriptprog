03d636e69fd27fd13d21f41fe41940d1b12c3be8
Saddam <saddamhussain1919@gmail.com>
2025-09-15 01:17:32 +0300

    added

--- reduce.js
+++ reduce.js
@@ -36,11 +36,11 @@ console.log([50].reduce(getMax, 10)); // 50
 console.log([1000, 100].reduce(getMax)); // 100
 // ↑ Uses 1 as starting value, calls getMax(1, 100) once
 
+
+
 // Empty array + no initial value = ERROR!
-console.log([].reduce(getMax)); // TypeError
+//console.log([].reduce(getMax)); // TypeError
 // ↑ "I have nothing to work with!"
-
-
 
 
 const arr = [15, 16, 17, 18, 19];
@@ -54,3 +54,36 @@ function reducer(accumulator, currentValue, index) {
 }
 
 arr.reduce(reducer);
+
+
+
+const objects = [{ x: 1 }, { x: 2 }, { x: 3 }];
+const sum = objects.reduce(
+  (accumulator, currentValue) => accumulator + currentValue.x,
+  0,
+);
+
+console.log(sum); // 6
+
+
+const pipe =
+  (...functions) =>
+  (initialValue) =>
+    functions.reduce((acc, fn) => fn(acc), initialValue);
+
+// Building blocks to use for composition
+const double = (x) => 2 * x;
+const triple = (x) => 3 * x;
+const quadruple = (x) => 4 * x;
+
+// Composed functions for multiplication of specific values
+const multiply6 = pipe(double, triple);
+const multiply9 = pipe(triple, triple);
+const multiply16 = pipe(quadruple, quadruple);
+const multiply24 = pipe(double, triple, quadruple);
+
+// Usage
+console.log(multiply6(6)); // 36
+console.log(multiply9(9)); // 81
+console.log(multiply16(16)); // 256
+console.log(multiply24(10)); // 240
